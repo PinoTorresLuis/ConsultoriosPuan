d54ff7fd45440b65677d47799e1b12bc379a3d7a
Pino Luis <pinotorres.luis@gmail.com>
2022-11-16 13:15:24 -0300

    se esta trabajando sobre el menu desplegable

--- src/Componentes/Navbar/Nav.jsx
+++ src/Componentes/Navbar/Nav.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars, faXmark} from '@fortawesome/free-solid-svg-icons' 
 import LogoTipo4 from '../../assets/logos/logotipo4.png';
 
 const Nav = () => {
+
+  const [show, setShow] = useState ();
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <nav>
       <div class="container-navegador">
         <div class="navbar-nav">        
         <div class="imagen-logo">
          <a href="index.html"><img class="imgNav" src= {LogoTipo4}/></a> 
-         <h4 class="titulo-navbar">DENTAL <b class="negrita-titulo">PUÁN</b></h4>    
+         <h1 class="titulo-navbar">DENTAL <b class="negrita-titulo">PUÁN</b></h1>    
         </div>
         <div> 
-        <i class="fa-solid fa-bars" id="btn-menu"></i>
+        <i id="btn-menu" onClick={handleShow}><FontAwesomeIcon className="fa-solid"  icon= {faBars}/></i>
+        <i id="btn-close" onClick={handleClose}><FontAwesomeIcon className="fa-solid"  icon= {faXmark}/></i>
         </div>    
-        <div class="menu-navbar collapse-navbar" id="MenuNavegacion">
+        <div class="menu-navbar collapse-navbar" id="MenuNavegacion" show={show}>
           <div class="container-nav">
               <li class="nav-item"><a class="nav-link-item" href="#inicio"> Inicio</a></li> 
               <li class="nav-item"><a class="nav-link-item" href="#tratamientos">Tratamientos</a></li>
